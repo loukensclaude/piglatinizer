48907ef46c303b457c9732e04847826828d0b4a5
loukensclaude <l.claude@ihsph.org>
2019-11-26 02:25:11 +0000

    can translate one word

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("body").ready(function(){
+
+    $(".button").click(function(){
+  let inputText = $(".input").val();
+  let lowerCase = inputText.toLowerCase();  
+  let splitWord = lowerCase.split(" ");
+  splitWord.forEach(function(word){
+    let wordSplit = word.split("");
+    var removed = wordSplit.splice(0, 1);
+    wordSplit.push(removed);
+    wordSplit.push("ay");
+    $(".output").text(wordSplit.join([]));
+  });
+});
 
 
-
-  
-  
-  
-  
 });
